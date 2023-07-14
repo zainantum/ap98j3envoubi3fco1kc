22f16171787f4a25d5a2a1a8ab45dab56692e80e
Patrick Borowy <patrick.borowy@proton.me>
2023-07-15 00:05:37 +0200

    hotfixes

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -1,12 +1,19 @@
 from ap98j3envoubi3fco1kc import query
-from exorde_data.models import Item
+from exorde_data import Item
 import pytest
 
 
 @pytest.mark.asyncio
 async def test_query():
-    url = "https://www.reddit.com/r/announcements/comments/8qfw8l/protecting_the_free_and_open_internet_european/"
     results = []
-    async for result in query(url):
+    async for result in query(
+        {
+            "url_parameters": {
+                "keyword": "BTC",
+                "autonomous_subreddit_choice": 1,
+            }
+        }
+    ):
         assert isinstance(result, Item)
         results.append(result)
+    print(len(results))
