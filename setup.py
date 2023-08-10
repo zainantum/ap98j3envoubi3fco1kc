927b8526d08197213c6f3eba2cedde3b74c6e0cb
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-08-10 15:36:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.32",
+    version="0.0.50",
     packages=find_packages(),
-    install_requires=["lxml>=4.9.3", "exorde_data", "aiohttp"],
+    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
