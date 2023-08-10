7f5ba71ee5f1bb682ac41bb4099ceee2611f8ce1
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-08-10 15:37:42 +0200

    Update test_implementation.py

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -5,15 +5,21 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_query():
-    results = []
+    item_count = 0
     async for result in query(
         {
+            "keyword": "Macron",
+            "max_oldness_seconds":1000000,
+            "nb_subreddit_attempts": 3,
+            "maximum_items_to_collect": 25,
+            "skip_post_probability" : 0.1,
+            "new_layout_scraping_weight": 0.1,
             "url_parameters": {
-                "keyword": "BTC",
-                "autonomous_subreddit_choice": 1,
+                "keyword": "crypto",
+                "autonomous_subreddit_choice": 0,
             }
         }
     ):
         assert isinstance(result, Item)
-        results.append(result)
-    print(len(results))
+        item_count += 1
+    print("FOUND ",item_count," items")
