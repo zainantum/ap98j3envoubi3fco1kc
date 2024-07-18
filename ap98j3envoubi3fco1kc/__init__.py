8fbbd76cc5bb8dc33b01f2d24b9ed6dc8758e7fa
Zain <115614618+zainantum@users.noreply.github.com>
2024-07-18 18:19:13 +0700

    Update __init__.py

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -27,7 +27,9 @@ from wordsegment import load, segment
 load()
 
 USER_AGENT_LIST = [
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
 ]
 
 global MAX_EXPIRATION_SECONDS
