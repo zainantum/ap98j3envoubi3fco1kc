3a0b7943e389041aab11c3a5037883b8cd0ab099
Zain <115614618+zainantum@users.noreply.github.com>
2024-07-18 18:42:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.72",
+    version="0.0.73",
     packages=find_packages(),
     install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "python-dotenv"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
