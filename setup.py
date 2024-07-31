b90f6a98da54b63eaa7c9039f3f0f2f7cca0bd90
Zain <115614618+zainantum@users.noreply.github.com>
2024-07-31 13:42:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="ap98j3envoubi3fco1kc",
     version="0.0.81",
     packages=find_packages(),
-    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "python-dotenv"],
+    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "python-dotenv", "pytz"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
